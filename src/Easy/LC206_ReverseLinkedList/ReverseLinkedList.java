72357b32c7256c1ab0254f236d2df6e6005489b8
harigithub1 <m.haribabu26@gmail.com>
2026-02-23 01:34:20 +0530

    update

--- src/Easy/LC206_ReverseLinkedList/ReverseLinkedList.java
+++ src/Easy/LC206_ReverseLinkedList/ReverseLinkedList.java
@@ -1,18 +1,35 @@
 package Easy.LC206_ReverseLinkedList;
-
 public class ReverseLinkedList {
-
     // Reverse function (Iterative)
     public static ListNode reverseList(ListNode head) {
         ListNode prevNode = null;
         ListNode currNode = head;
-
         while (currNode != null) {
+            //Save the next node
+            ListNode nextNode = currNode.next;
+            //Reverse the pointer
+            currNode.next = prevNode;
+            //Move prevNode forward
+            prevNode = currNode;
+            //Move currNode forward using saved reference
+            currNode = nextNode;
+        }
+        return prevNode;
+    }
+    public static ListNode reverseList2(ListNode head) {
+        ListNode prevNode = head;
+        ListNode currNode = head.next;
+        while(currNode != null) {
             ListNode nextNode = currNode.next;
             currNode.next = prevNode;
+
+            //update
             prevNode = currNode;
             currNode = nextNode;
         }
+
+        head.next = null;
+        head = prevNode;
         return prevNode;
     }
 
@@ -42,5 +59,17 @@ public class ReverseLinkedList {
 
         System.out.println("Reversed List:");
         printList(reversedHead);
+
+        // Recreate the list
+        head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        // Reverse the list
+        ListNode reversedHead2 = reverseList2(head);
+
+        System.out.println("Reversed List2:");
+        printList(reversedHead2);
     }
 }
