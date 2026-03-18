c8992cd8cd30c177b590751cf49f2cde79326c14
harigithub1 <m.haribabu26@gmail.com>
2026-03-19 00:46:52 +0530

    update

---
+++ src/Easy/LC070_ClimbingStairs.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class LC070_ClimbingStairs {
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev2 = 1; // ways to reach step 1
+        int prev1 = 2; // ways to reach step 2
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String args[]){
+        int n = 2;
+        System.out.println(climbStairs(n));
+    }
+}
+
+/**
+ * 👉 Same pattern appears in:
+ *
+ * House Robber
+ *
+ * Min Cost Climbing Stairs
+ *
+ * Decode Ways
+ */
