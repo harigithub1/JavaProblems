503062b7a7f4c5f31f253942ea155d4f88452a21
harigithub1 <m.haribabu26@gmail.com>
2026-03-19 01:06:19 +0530

    update

---
+++ src/Easy/LC141_LinkedListCycle/LinkedListCycle.java
@@ -0,0 +1,38 @@
+package Easy.LC141_LinkedListCycle;
+
+public class LinkedListCycle {
+        public static boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) return false;
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;          // move 1 step
+                fast = fast.next.next;     // move 2 steps
+
+                if (slow == fast) {
+                    return true;           // cycle detected
+                }
+            }
+            return false;                  // no cycle
+        }
+
+        public static void main(String args[]){
+            ListNode head = new ListNode(3);
+            ListNode second = new ListNode(2);
+            ListNode third = new ListNode(0);
+            ListNode fourth = new ListNode(-4);
+            head.next=second;
+            second.next=third;
+            third.next=fourth;
+
+            int pos = 1;
+
+            // 🔥 Create cycle manually
+            if (pos == 0) fourth.next = head;
+            else if (pos == 1) fourth.next = second;
+            else if (pos == 2) fourth.next = third;
+            else if (pos == 3) fourth.next = fourth;
+
+            System.out.println(hasCycle(head));
+        }
+    }
