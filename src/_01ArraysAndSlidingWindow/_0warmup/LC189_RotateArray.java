bdba6c9f052edaa447758336fd6f8750c3b0ab69
harigithub1 <m.haribabu26@gmail.com>
2026-04-07 00:10:53 +0530

    fixes

--- src/_01ArraysAndSlidingWindow/_0warmup/LC189_RotateArray.java
+++ src/_01ArraysAndSlidingWindow/_0warmup/LC189_RotateArray.java
@@ -2,11 +2,11 @@ package _01ArraysAndSlidingWindow._0warmup;
 
 public class LC189_RotateArray {
     public static void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; // to handle cases where k > n
-        reverse(nums, 0, n - 1);         // Step 1: Reverse the whole array
+        int l = nums.length;
+        k = k % l; // to handle cases where k > n
+        reverse(nums, 0, l - 1);         // Step 1: Reverse the whole array
         reverse(nums, 0, k - 1);         // Step 2: Reverse first k elements
-        reverse(nums, k, n - 1);         // Step 3: Reverse remaining n - k elements
+        reverse(nums, k, l - 1);         // Step 3: Reverse remaining n - k elements
     }
 
     private static void reverse(int[] nums, int start, int end) {
--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,26 +1,40 @@
 package practice;
 
-/**
- *           1
- *          / \
- *         2   3
- *        / \
- *       4   5
- */
+class Main {
+    public static void rotate(int nums[], int k) {
+        int l = nums.length;
+         k=k%l;
+        reverse(nums, 0, l);
+        for (int n : nums) {
+            System.out.print(n);
+        }
+        System.out.println();
 
-public class Main{
-    public static void preOrder(TreeNode root){
-        if(root==null) return;
-        preOrder(root.left);
-        System.out.println(root.val);
-        preOrder(root.right);
+        reverse(nums, 0, k);
+        for (int n : nums) {
+            System.out.print(n);
+        }
+        System.out.println();
+
+        reverse(nums, k, l);
     }
-    public static void main(String args[]){
-        TreeNode root = new TreeNode(1);
-        root.left=new TreeNode(2);
-        root.right=new TreeNode(3);
-        root.left.left = new TreeNode(4);
-        root.left.right=new TreeNode(5);
-        preOrder(root);
+
+    public static void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j - 1];
+            nums[j - 1] = temp;
+            j--;
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotate(nums, k);
+        for (int n : nums) {
+            System.out.print(n);
+        }
     }
 }
