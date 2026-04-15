ff72ba1ac7b73e5ea5b483bda817e35c49e3f85b
harigithub1 <m.haribabu26@gmail.com>
2026-04-16 01:42:00 +0530

    fix

---
+++ src/_01ArraysAndHashing/Medium4_LC049_GroupAnagrams.java
@@ -0,0 +1,25 @@
+package _01ArraysAndHashing;
+import java.util.*;
+
+public class Medium4_LC049_GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> result = groupAnagrams(input);
+        // Print result
+        for (List<String> group : result) {
+            System.out.println(group);
+        }
+    }
+}
