733fe3b4fea1898d13a3feddc625d57a6bfdcffd
harigithub1 <m.haribabu26@gmail.com>
2026-04-19 13:43:10 +0530

    fixes

--- src/_01ArraysAndHashing/Medium2_LC347_TopKFrequentElements_Beginner.java
+++ src/_01ArraysAndHashing/Medium2_LC347_TopKFrequentElements_Beginner.java
@@ -4,27 +4,27 @@ import java.util.*;
 public class Medium2_LC347_TopKFrequentElements_Beginner {
 
     public static int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        List<int[]> arr = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        List<int[]> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(new int[] {entry.getValue(), entry.getKey()});
         }
-        arr.sort((a, b) -> b[0] - a[0]);
+        list.sort((a, b) -> b[0] - a[0]);
 
-        int[] res = new int[k];
+        int[] arr = new int[k];
         for (int i = 0; i < k; i++) {
-            res[i] = arr.get(i)[1];
+            arr[i] = list.get(i)[1];
         }
-        return res;
+        return arr;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1,1,1,2,2,3};
-        int k1 = 2;
-        System.out.println(Arrays.toString(topKFrequent(nums1, k1)));
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
     }
 }
