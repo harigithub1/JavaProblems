2b5586385d21a7c648445831a3d8c16c645d676a
harigithub1 <m.haribabu26@gmail.com>
2026-04-19 15:20:17 +0530

    fixes

--- src/_01ArraysAndHashing/Medium3_EncodeAndDecodeStrings.java
+++
@@ -1,4 +0,0 @@
-package _01ArraysAndHashing;
-
-public class Medium3_EncodeAndDecodeStrings {
-}
---
+++ src/_01ArraysAndHashing/Medium3_EncodeAndDecodeStrings_Advanced.java
@@ -0,0 +1,57 @@
+package _01ArraysAndHashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Medium3_EncodeAndDecodeStrings_Advanced {
+
+    // Encodes a list of strings to a single string.
+    public static String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : strs) {
+            sb.append(s.length()).append('#').append(s);
+        }
+
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public static List<String> decode(String s) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+
+        while (i < s.length()) {
+            int j = i;
+
+            // Find the delimiter '#'
+            while (s.charAt(j) != '#') {
+                j++;
+            }
+
+            int length = Integer.parseInt(s.substring(i, j));
+            j++; // skip '#'
+
+            String word = s.substring(j, j + length);
+            result.add(word);
+
+            i = j + length;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Input
+        List<String> input = Arrays.asList("leet", "code", "love", "you");
+
+        // Encode
+        String encoded = encode(input);
+        System.out.println("Encoded: " + encoded);
+
+        // Decode
+        List<String> decoded = decode(encoded);
+        System.out.println("Decoded: " + decoded);
+    }
+}
