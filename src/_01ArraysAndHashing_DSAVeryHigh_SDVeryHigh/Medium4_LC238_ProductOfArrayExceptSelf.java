fce2e1ba32a2f95fc230d6a614457ae82e6ebf6c
harigithub1 <m.haribabu26@gmail.com>
2026-04-21 00:01:18 +0530

    main

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium4_LC238_ProductOfArrayExceptSelf.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium4_LC238_ProductOfArrayExceptSelf.java
@@ -12,7 +12,7 @@ public class Medium4_LC238_ProductOfArrayExceptSelf {
         // Step 2: Suffix product (from right)
         int suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
-            result[i] *= suffix;
+            result[i] = result[i]*suffix;
             suffix *= nums[i];
         }
         return result;
@@ -26,6 +26,19 @@ public class Medium4_LC238_ProductOfArrayExceptSelf {
 }
 /**
  * Prefix–Suffix (Cumulative Product) Pattern
+ *
+ * nums = [1, 2, 3, 4]
+ * result[0]=1
+ * first for loop
+ * i=1, result[1]=1*1=1
+ * i=2, result[2]=1*2=2
+ * i=3, result[3]=2*3=6
+ * suffix=1
+ * second for loop
+ * i=3, result[3]=6*1=6, suffix=1*4=4
+ * i=2, result[2]=2*4=8, suffix=4*3=12
+ * i=1, result[1]=1*12=12, suffix=12*2=24
+ * i=0, result[0]=1*24=24, suffix=24*1=24
  *
  * 🧠 Related Patterns
  * This problem belongs to a broader category:
