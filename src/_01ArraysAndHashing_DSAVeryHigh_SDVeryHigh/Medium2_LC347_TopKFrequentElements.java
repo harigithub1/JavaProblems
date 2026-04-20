0bd16d5aca0064b5b0eff22c5d207aa2cdb9e334
harigithub1 <m.haribabu26@gmail.com>
2026-04-21 00:30:57 +0530

    updates

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium2_LC347_TopKFrequentElements.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium2_LC347_TopKFrequentElements.java
@@ -1,56 +1,36 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh;
+
 import java.util.*;
 
 public class Medium2_LC347_TopKFrequentElements {
     public static int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count frequency
-        Map<Integer, Integer> freqMap = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
         for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Bucket sort (index = frequency)
-        List<Integer>[] bucket = new List[nums.length + 1];
-        for (int key : freqMap.keySet()) {
-            int freq = freqMap.get(key);
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(key);
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[]{entry.getValue(), entry.getKey()});
         }
+        arr.sort((a, b) -> b[0] - a[0]);
 
-        System.out.println("bucket");
-        for(List<Integer> l: bucket) {
-            System.out.println(l);
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
         }
-
-        // Step 3: Collect top k elements
-        List<Integer> result = new ArrayList<>();
-        for (int i = bucket.length - 1; i >= 0 && result.size() < k; i--) {
-            if (bucket[i] != null) {
-                result.addAll(bucket[i]);
-            }
-        }
-
-        System.out.println("result");
-        for(List<Integer> l: bucket) {
-            System.out.println(l);
-        }
-
-        // Convert to array
-        return result.stream().limit(k).mapToInt(i -> i).toArray();
+        return res;
     }
 
     public static void main(String[] args) {
 
         // Example 1
-        int[] nums1 = {1,1,1,2,2,3};
+        int[] nums1 = {1, 1, 1, 2, 2, 3};
         int k1 = 2;
         System.out.println(Arrays.toString(topKFrequent(nums1, k1)));
     }
 }
 /**
- *
  * Step 3: Collect top k elements (because bucket may contain empty check below comments [ null, [3], [2], [1], null, null, null] for input         int[] nums1 = {1,1,1,2,2,3}; )
  * bucket[0] = null
  * bucket[1] = [3]
