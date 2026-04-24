8c38be09877659253b990f03fe0d4d36183a25d4
harigithub1 <m.haribabu26@gmail.com>
2026-04-25 00:01:42 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium5_LC036_ValidSudoku.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium5_LC036_ValidSudoku.java
@@ -1,24 +1,19 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh;
 
-import java.util.*;
-
 public class Medium5_LC036_ValidSudoku {
     public static boolean isValidSudoku(char[][] board) {
         int[] rows = new int[9];
         int[] cols = new int[9];
         int[] squares = new int[9];
-
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] == '.') continue;
-
                 int val = board[r][c] - '1';
-
-                if ((rows[r] & (1 << val)) > 0 || (cols[c] & (1 << val)) > 0 ||
+                if (    (rows[r] & (1 << val)) > 0 ||
+                        (cols[c] & (1 << val)) > 0 ||
                         (squares[(r / 3) * 3 + (c / 3)] & (1 << val)) > 0) {
                     return false;
                 }
-
                 rows[r] |= (1 << val);
                 cols[c] |= (1 << val);
                 squares[(r / 3) * 3 + (c / 3)] |= (1 << val);
@@ -27,9 +22,7 @@ public class Medium5_LC036_ValidSudoku {
         return true;
     }
 
-
     public static void main(String[] args) {
-
         char[][] board = {
                 {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
                 {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
@@ -41,9 +34,7 @@ public class Medium5_LC036_ValidSudoku {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
-
         boolean result = isValidSudoku(board);
-
         System.out.println("Is valid Sudoku? " + result);
     }
 }
