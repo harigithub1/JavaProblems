b66ac3f9a6c8b845fbb9fbc7764310843ed7d3cf
harigithub1 <m.haribabu26@gmail.com>
2026-04-25 00:23:33 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium5_LC036_ValidSudoku.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium5_LC036_ValidSudoku.java
@@ -1,22 +1,28 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh;
 
+import java.util.*;
+
 public class Medium5_LC036_ValidSudoku {
     public static boolean isValidSudoku(char[][] board) {
-        int[] rows = new int[9];
-        int[] cols = new int[9];
-        int[] squares = new int[9];
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
+
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] == '.') continue;
-                int val = board[r][c] - '1';
-                if (    (rows[r] & (1 << val)) > 0 ||
-                        (cols[c] & (1 << val)) > 0 ||
-                        (squares[(r / 3) * 3 + (c / 3)] & (1 << val)) > 0) {
+
+                String squareKey = (r / 3) + "," + (c / 3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                        cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                        squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
                     return false;
                 }
-                rows[r] |= (1 << val);
-                cols[c] |= (1 << val);
-                squares[(r / 3) * 3 + (c / 3)] |= (1 << val);
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
             }
         }
         return true;
