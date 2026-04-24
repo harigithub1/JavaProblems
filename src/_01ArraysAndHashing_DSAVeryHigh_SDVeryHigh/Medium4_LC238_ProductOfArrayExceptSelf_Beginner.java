f945806266590054b57f2df394b6943f56f6af05
harigithub1 <m.haribabu26@gmail.com>
2026-04-24 23:41:52 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium4_LC238_ProductOfArrayExceptSelf_Beginner.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium4_LC238_ProductOfArrayExceptSelf_Beginner.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public class Medium4_LC238_ProductOfArrayExceptSelf_Beginner {
     public static int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        int[] pref = new int[n];
-        int[] suff = new int[n];
+        int l = nums.length;
+        int[] res = new int[l];
+        int[] pref = new int[l];
+        int[] suff = new int[l];
         System.out.println(Arrays.toString(nums));
 
         pref[0] = 1;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < l; i++) {
             pref[i] = nums[i - 1] * pref[i - 1];
         }
-        suff[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
+        suff[l - 1] = 1;
+        for (int i = l - 2; i >= 0; i--) {
             suff[i] = nums[i + 1] * suff[i + 1];
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < l; i++) {
             res[i] = pref[i] * suff[i];
         }
         System.out.println(Arrays.toString(res));
