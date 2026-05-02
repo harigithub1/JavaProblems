f57c17439fdac27bc277e6713f05926ac57b19e1
harigithub1 <m.haribabu26@gmail.com>
2026-05-02 22:48:23 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Easy3_LC001_TwoSum.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Easy3_LC001_TwoSum.java
@@ -7,9 +7,8 @@ public class Easy3_LC001_TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
             map.put(nums[i], i);
         }
