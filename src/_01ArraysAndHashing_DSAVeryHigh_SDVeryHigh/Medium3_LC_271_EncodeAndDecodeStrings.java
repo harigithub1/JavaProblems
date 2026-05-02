ff8db23be4fe671b9bedb390218ec5c5e7e5420d
harigithub1 <m.haribabu26@gmail.com>
2026-05-03 00:06:58 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium3_LC_271_EncodeAndDecodeStrings.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/Medium3_LC_271_EncodeAndDecodeStrings.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Medium3_LC_271_EncodeAndDecodeStrings {
-    // Encodes a list of strings to a single string.
     public static String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
         for (String s : strs) {
@@ -14,13 +13,11 @@ public class Medium3_LC_271_EncodeAndDecodeStrings {
         return sb.toString();
     }
 
-    // Decodes a single string to a list of strings.
     public static List<String> decode(String s) {
         List<String> result = new ArrayList<>();
         int i = 0;
         while (i < s.length()) {
             int j = i;
-            // Find the delimiter '#'
             while (s.charAt(j) != '#') {
                 j++;
             }
@@ -32,12 +29,9 @@ public class Medium3_LC_271_EncodeAndDecodeStrings {
     }
 
     public static void main(String[] args) {
-        // Input
         List<String> input = Arrays.asList("hello", "world");
-        // Encode
         String encoded = encode(input);
         System.out.println("Encoded: " + encoded);
-        // Decode
         List<String> decoded = decode(encoded);
         System.out.println("Decoded: " + decoded);
     }
