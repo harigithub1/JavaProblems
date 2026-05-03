27f53b4eed404eaa8a01d870fca20ce26c604e0e
harigithub1 <m.haribabu26@gmail.com>
2026-05-04 01:47:42 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy1_LC1929_ConcatenationofArray.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy1_LC1929_ConcatenationofArray.java
@@ -1,4 +1,22 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
+import java.util.Arrays;
+
 public class Easy1_LC1929_ConcatenationofArray {
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        int[] result = getConcatenation(nums);
+        System.out.println("Input: " + Arrays.toString(nums));
+        System.out.println("Output: " + Arrays.toString(result));
+    }
 }
--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy5_LC014_LongestCommonPrefix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy5_LC014_LongestCommonPrefix.java
@@ -1,4 +1,19 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
 public class Easy5_LC014_LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (String s : strs) {
+                if (i == s.length() || s.charAt(i) != strs[0].charAt(i)) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        String result = longestCommonPrefix(strs);
+        System.out.println("Longest Common Prefix: " + result);
+    }
 }
