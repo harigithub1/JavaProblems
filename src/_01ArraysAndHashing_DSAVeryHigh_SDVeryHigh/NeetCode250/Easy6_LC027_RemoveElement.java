6418ddfbe30464169a7d362298d3c8f960b63056
harigithub1 <m.haribabu26@gmail.com>
2026-05-04 01:52:30 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy6_LC027_RemoveElement.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy6_LC027_RemoveElement.java
@@ -1,4 +1,24 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
 public class Easy6_LC027_RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; // slow pointer
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int k = removeElement(nums, val);
+        System.out.println("k = " + k);
+        System.out.print("Updated array (first k elements): ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
