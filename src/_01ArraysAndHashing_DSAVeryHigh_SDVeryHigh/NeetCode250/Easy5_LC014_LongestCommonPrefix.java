7b50871616588061ddded0e4e03414dc07189af6
harigithub1 <m.haribabu26@gmail.com>
2026-05-06 01:29:37 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy5_LC014_LongestCommonPrefix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy5_LC014_LongestCommonPrefix.java
@@ -17,3 +17,45 @@ public class Easy5_LC014_LongestCommonPrefix {
         System.out.println("Longest Common Prefix: " + result);
     }
 }
+/**
+ * i=0
+ *
+ * flower
+ * s.charAt(0) f
+ * strs[0].charAt(0) f
+ *
+ * flow
+ * s.charAt(0) f
+ * strs[0].charAt(0) f
+ *
+ * flight
+ * s.charAt(0) f
+ * strs[0].charAt(0) f
+ *
+ * flower
+ * s.charAt(1) l
+ * strs[0].charAt(1) l
+ *
+ * flow
+ * s.charAt(1) l
+ * strs[0].charAt(1) l
+ *
+ * flight
+ * s.charAt(1) l
+ * strs[0].charAt(1) l
+ *
+ * flower
+ * s.charAt(2) o
+ * strs[0].charAt(2) o
+ *
+ * flow
+ * s.charAt(2) o
+ * strs[0].charAt(2) o
+ *
+ * flight
+ * s.charAt(2) i
+ * strs[0].charAt(2) o
+ *
+ * return s.substring(0,i)
+ * return s.substring(0,2)
+ */
