51d88d98ffc20367bcbeb9abc7ae8df6ffdac537
harigithub1 <m.haribabu26@gmail.com>
2026-05-10 01:54:22 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy4_LeetCode001_TwoSum.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy4_LeetCode001_TwoSum.java
@@ -7,9 +7,9 @@ public class Easy4_LeetCode001_TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int difference = target - nums[i];
-            if (map.containsKey(difference)) {
-                return new int[]{map.get(difference), i};
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
             }
             map.put(nums[i], i);
         }
