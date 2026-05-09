a25cfdc5947f7bca078e53843389b2f81f074107
harigithub1 <m.haribabu26@gmail.com>
2026-05-10 01:27:51 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy1_LeetCode1929_ConcatenationofArray.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy1_LeetCode1929_ConcatenationofArray.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Easy1_LeetCode1929_ConcatenationofArray {
     public static int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[2 * n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = nums[i];
-            ans[i + n] = nums[i];
+        int l = nums.length;
+        int[] res = new int[2 * l];
+        for (int i = 0; i < l; i++) {
+            res[i] = nums[i];
+            res[i + l] = nums[i];
         }
-        return ans;
+        return res;
     }
 
     public static void main(String[] args) {
