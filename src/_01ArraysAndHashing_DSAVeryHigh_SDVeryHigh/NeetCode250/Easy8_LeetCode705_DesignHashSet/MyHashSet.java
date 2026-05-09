005654e9afd58e1bb11840ef9cde9b2c6cf77298
harigithub1 <m.haribabu26@gmail.com>
2026-05-10 00:58:18 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy8_LeetCode705_DesignHashSet/MyHashSet.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy8_LeetCode705_DesignHashSet/MyHashSet.java
@@ -10,17 +10,17 @@ public class MyHashSet {
         data = new ArrayList<>();
     }
 
-    public void add(int key) {
-        if (!data.contains(key)) {
-            data.add(key);
+    public void add(int val) {
+        if (!data.contains(val)) {
+            data.add(val);
         }
     }
 
-    public void remove(int key) {
-        data.remove(Integer.valueOf(key));
+    public void remove(int val) {
+        data.remove(Integer.valueOf(val));
     }
 
-    public boolean contains(int key) {
-        return data.contains(key);
+    public boolean contains(int val) {
+        return data.contains(val);
     }
 }
