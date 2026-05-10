8933bddf9c4fc7211c65d2d86d6bc65f41eaf33a
harigithub1 <m.haribabu26@gmail.com>
2026-05-10 23:08:13 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Medium2_LeetCode912_SortAnArray.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Medium2_LeetCode912_SortAnArray.java
@@ -1,5 +1,61 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
-public class Medium2_LeetCode912_SortAnArray {
-}
+import java.util.*;
 
+//merge sort
+public class Medium2_LeetCode912_SortAnArray {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+
+    private void merge(int[] arr, int l, int m, int r) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int i = l;
+        int j = m + 1;
+
+        while (i <= m && j <= r) {
+            if (arr[i] <= arr[j]) {
+                temp.add(arr[i]);
+                i++;
+            } else {
+                temp.add(arr[j]);
+                j++;
+            }
+        }
+
+
+        while (i <= m) {
+            temp.add(arr[i]);
+            i++;
+        }
+
+        while (j <= r) {
+            temp.add(arr[j]);
+            j++;
+        }
+
+        for (i = l; i <= r; i++) {
+            arr[i] = temp.get(i - l);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Medium2_LeetCode912_SortAnArray s = new Medium2_LeetCode912_SortAnArray();
+
+        int[] nums = {5, 2, 3, 1};
+
+        int[] result = s.sortArray(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
