6da2ced9a83121b617fc46825ea81a9501812d4b
harigithub1 <m.haribabu26@gmail.com>
2026-05-10 22:29:09 +0530

    update

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy9_LeetCode706_DesignHashMap/Main.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/Easy9_LeetCode706_DesignHashMap/Main.java
@@ -1,4 +1,24 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250.Easy9_LeetCode706_DesignHashMap;
 
 public class Main {
-}
+    public static void main(String[] args) {
+
+        MyHashMap myHashMap = new MyHashMap();
+
+        // put operations
+        myHashMap.put(1, 10);
+        myHashMap.put(2, 20);
+
+        // get operations
+        System.out.println(myHashMap.get(1)); // 10
+        System.out.println(myHashMap.get(2)); // 20
+        System.out.println(myHashMap.get(3)); // -1
+
+        // update value
+        myHashMap.put(2, 50);
+        System.out.println(myHashMap.get(2)); // 50
+
+        // remove operation
+        myHashMap.remove(2);
+        System.out.println(myHashMap.get(2)); // -1
+    }}
