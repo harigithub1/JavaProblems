1f3f455a884188f53c2c4181b1a6f5e4226f66f2
harigithub1 <m.haribabu26@gmail.com>
2026-05-12 14:34:27 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Beginner.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Beginner.java
@@ -2,49 +2,49 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
 //merge sort
 public class _11_Medium_LeetCode912_SortAnArray_Beginner {
-    public static void conquer(int[] arr, int si, int mid, int ei) {
-        int[] merged = new int[ei - si + 1];
+    public static void merge(int[] arr, int si, int mid, int ei) {
+        int[] temp = new int[ei - si + 1];
         int idx1 = si;
         int idx2 = mid + 1;
         int x = 0;
         while (idx1 <= mid && idx2 <= ei) {
             if (arr[idx1] <= arr[idx2]) {
-                merged[x] = arr[idx1];
+                temp[x] = arr[idx1];
                 x++;
                 idx1++;
             } else {
-                merged[x] = arr[idx2];
+                temp[x] = arr[idx2];
                 x++;
                 idx2++;
             }
         }
         while (idx1 <= mid) {
-            merged[x] = arr[idx1];
+            temp[x] = arr[idx1];
             x++;
             idx1++;
         }
         while (idx2 <= ei) {
-            merged[x] = arr[idx2];
+            temp[x] = arr[idx2];
             x++;
             idx2++;
         }
-        for (int i = 0, j = si; i < merged.length; i++, j++) {
-            arr[j] = merged[i];
+        for (int i = 0, j = si; i < temp.length; i++, j++) {
+            arr[j] = temp[i];
         }
     }
 
-    public static void divide(int[] arr, int si, int ei) {
+    public static void mergeSort(int[] arr, int si, int ei) {
         if (si >= ei) return;
         int mid = si + (ei - si) / 2;
-        divide(arr, si, mid);
-        divide(arr, mid + 1, ei);
-        conquer(arr, si, mid, ei);
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
     }
 
     public static void main(String[] args) {
         int[] arr = {6, 3, 9, 5, 2, 8};
         int n = arr.length;
-        divide(arr, 0, n - 1);
+        mergeSort(arr, 0, n - 1);
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
