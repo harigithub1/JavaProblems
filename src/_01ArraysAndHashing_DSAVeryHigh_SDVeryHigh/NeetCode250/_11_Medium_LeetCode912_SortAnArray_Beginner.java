983b968b7656d534f822b7414a137e5365c42bd2
harigithub1 <m.haribabu26@gmail.com>
2026-05-15 23:12:02 +0530

    main

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Beginner.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Beginner.java
@@ -48,6 +48,64 @@ public class _11_Medium_LeetCode912_SortAnArray_Beginner {
     }
 }
 /*
+arr={6,3,9,5,2,8}
+mergeSort(arr,0,5)
+mergeSort(arr,0,2)
+mergeSort(arr,0,1)
+mergeSort(arr,0,0) return
+mergeSort(arr,1,1) return
+merge(arr,0,0,1)
+i=0,m=0,j=1,r=1
+temp ={3}
+i=0,m=0,j=2,r=1
+temp={3,6}
+arr=	{3,6,9,5,2,8}
+//mergeSort(arr,0,2)
+mergeSort(arr,2,2) return
+merge(arr,0,1,2)
+i=0,m=1,j=2,r=2
+temp={3}
+i=1,m=1,j=2,r=2
+temp={3,6}
+i=2,m=1,j=2,r=2
+temp={3,6,9}
+arr=	{3,6,9,5,2,8}
+mergeSort(arr,3,5)
+mergeSort(arr,3,4)
+mergeSort(arr,3,3) return
+mergeSort(arr,4,4) return
+//mergeSort(arr,3,4)
+merge(arr,3,3,4)
+i=3,m=3,j=4,r=4
+temp={2}
+i=3,m=3,j=5,r=4
+temp={2,5}
+arr=	{3,6,9,2,5,8}
+//mergeSort(arr,3,5)
+mergeSort(arr,5,5) return
+merge(arr,3,4,5)
+i=3,m=4,j=5,r=5
+temp={2}
+i=4,m=4,j=5,r=5
+temp={2,5}
+i=5,m=4,j=5,r=5
+temp={2,5,8}
+arr=	{3,6,9,2,5,8}
+//mergeSort(arr,0,5)
+merge(arr,0,2,5)
+i=0,m=2,j=3,r=5
+temp={2}
+i=0,m=2,j=4,r=5
+temp={2,3}
+i=1,m=2,j=4,r=5
+temp={2,3,5}
+i=1,m=2,j=5,r=5
+temp={2,3,5,6}
+i=2,m=2,j=5,r=5
+temp={2,3,5,6,8}
+i=2,m=2,j=6,r=5
+temp={2,3,5,6,8,9}
+
 One Important Mental Model
 
 Think of merge sort as:
