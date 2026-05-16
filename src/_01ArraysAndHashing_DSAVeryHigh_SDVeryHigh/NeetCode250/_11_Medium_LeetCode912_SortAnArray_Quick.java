8569e92e2054bd845f4967261c8f93e0c0462dcf
harigithub1 <m.haribabu26@gmail.com>
2026-05-17 01:38:25 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Quick.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_11_Medium_LeetCode912_SortAnArray_Quick.java
@@ -3,50 +3,50 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 import java.util.*;
 //quick sort
 public class _11_Medium_LeetCode912_SortAnArray_Quick {
-    private static int partition(int[] nums, int l, int r) {
+    private static int partition(int[] arr, int l, int r) {
         int mid = (l + r)/2;
-        swap(nums, mid, l + 1);
-        if (nums[l] > nums[r])
-            swap(nums, l, r);
-        if (nums[l + 1] > nums[r])
-            swap(nums, l + 1, r);
-        if (nums[l] > nums[l + 1])
-            swap(nums, l, l + 1);
-        int pivot = nums[l + 1];
+        swap(arr, mid, l + 1);
+        if (arr[l] > arr[r])
+            swap(arr, l, r);
+        if (arr[l + 1] > arr[r])
+            swap(arr, l + 1, r);
+        if (arr[l] > arr[l + 1])
+            swap(arr, l, l + 1);
+        int pivot = arr[l + 1];
         int i = l + 1;
         int j = r;
         while (true) {
             i = i + 1;
-            while (nums[i] < pivot) {
+            while (arr[i] < pivot) {
                 i = i + 1;
             }
             j = j - 1;
-            while (nums[j] > pivot) {
+            while (arr[j] > pivot) {
                 j = j - 1;
             }
             if (i > j) break;
-            swap(nums, i, j);
+            swap(arr, i, j);
         }
-        nums[l + 1] = nums[j];
-        nums[j] = pivot;
+        arr[l + 1] = arr[j];
+        arr[j] = pivot;
         return j;
     }
 
-    private static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    private static void quickSort(int[] nums, int l, int r) {
+    private static void quickSort(int[] arr, int l, int r) {
         if (r <= l + 1) {
-            if (r == l + 1 && nums[r] < nums[l])
-                swap(nums, l, r);
+            if (r == l + 1 && arr[r] < arr[l])
+                swap(arr, l, r);
             return;
         }
-        int j = partition(nums, l, r);
-        quickSort(nums, l, j - 1);
-        quickSort(nums, j + 1, r);
+        int j = partition(arr, l, r);
+        quickSort(arr, l, j - 1);
+        quickSort(arr, j + 1, r);
     }
 
     public static int[] sortArray(int[] nums) {
