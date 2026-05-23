bb5fdf8b0a681945e028e3eeade127bcd2395c4f
harigithub1 <m.haribabu26@gmail.com>
2026-05-23 07:42:06 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_05_Easy_LeetCode014_LongestCommonPrefix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_05_Easy_LeetCode014_LongestCommonPrefix.java
@@ -1,6 +1,11 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
 public class _05_Easy_LeetCode014_LongestCommonPrefix {
+    /**
+     * vertical scan / column-wise traversal pattern.
+     * @param strs
+     * @return longest common prefix
+     */
     public static String longestCommonPrefix(String[] strs) {
         for (int i = 0; i < strs[0].length(); i++) {
             for (String s : strs) {
@@ -17,45 +22,3 @@ public class _05_Easy_LeetCode014_LongestCommonPrefix {
         System.out.println("Longest Common Prefix: " + result);
     }
 }
-/**
- *
- * flower
- * s.charAt(0) f
- * strs[0].charAt(0) f
- *
- * flow
- * s.charAt(0) f
- * strs[0].charAt(0) f
- *
- * flight
- * s.charAt(0) f
- * strs[0].charAt(0) f
- *
- * flower
- * s.charAt(1) l
- * strs[0].charAt(1) l
- *
- * flow
- * s.charAt(1) l
- * strs[0].charAt(1) l
- *
- * flight
- * s.charAt(1) l
- * strs[0].charAt(1) l
- *
- * flower
- * s.charAt(2) o
- * strs[0].charAt(2) o
- *
- * flow
- * s.charAt(2) o
- * strs[0].charAt(2) o
- *
- * flight
- * s.charAt(2) i
- * strs[0].charAt(2) o
- *
- * return s.substring(0,i)
- * return s.substring(0,2)
- */
-
