6cdc19d4b9b75b510b8bffe5f1a2a5f0c980f4d4
harigithub1 <m.haribabu26@gmail.com>
2026-05-24 21:34:38 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_12_Medium_LeetCode075_SortColors.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_12_Medium_LeetCode075_SortColors.java
@@ -1,5 +1,32 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250;
 
+import java.util.Arrays;
+//Three Pointers - I
 public class _12_Medium_LeetCode075_SortColors {
-}
+    public static void sortColors(int[] nums) {
+        int i = 0, l = 0, r = nums.length - 1;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                swap(nums, l, i);
+                l++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, r);
+                r--;
+                i--;
+            }
+            i++;
+        }
+    }
 
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,40 +1,6 @@
 package practice;
-
-class Main {
-    public static void rotate(int nums[], int k) {
-        int l = nums.length;
-         k=k%l;
-        reverse(nums, 0, l);
-        for (int n : nums) {
-            System.out.print(n);
-        }
-        System.out.println();
-
-        reverse(nums, 0, k);
-        for (int n : nums) {
-            System.out.print(n);
-        }
-        System.out.println();
-
-        reverse(nums, k, l);
-    }
-
-    public static void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j - 1];
-            nums[j - 1] = temp;
-            j--;
-            i++;
-        }
-    }
-
+class Main{
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        rotate(nums, k);
-        for (int n : nums) {
-            System.out.print(n);
-        }
+        MyHashMap map = new MyHashMap();
     }
 }
