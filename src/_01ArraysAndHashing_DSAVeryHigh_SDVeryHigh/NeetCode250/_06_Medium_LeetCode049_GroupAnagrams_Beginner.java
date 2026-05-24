df0270b4453f367022bf373a695cd77628992b04
harigithub1 <m.haribabu26@gmail.com>
2026-05-24 15:36:09 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LeetCode049_GroupAnagrams_Beginner.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LeetCode049_GroupAnagrams_Beginner.java
@@ -6,9 +6,9 @@ public class _06_Medium_LeetCode049_GroupAnagrams_Beginner {
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] c = s.toCharArray();
-            Arrays.sort(c);
-            String key = new String(c);
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
             map.putIfAbsent(key, new ArrayList<>());
             map.get(key).add(s);
         }
