5b09e4638930c14973bbfc13b37528d01f8913d7
harigithub1 <m.haribabu26@gmail.com>
2026-06-05 11:06:19 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_13_Medium_LC347_TopKFrequentElements_B_BucketSort.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_13_Medium_LC347_TopKFrequentElements_B_BucketSort.java
@@ -9,18 +9,18 @@ public class _13_Medium_LC347_TopKFrequentElements_B_BucketSort {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        List<Integer>[] list = new List[nums.length + 1];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = new ArrayList<>();
+        List<Integer>[] arr = new List[nums.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = new ArrayList<>();
         }
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            list[entry.getValue()].add(entry.getKey());
+            arr[entry.getValue()].add(entry.getKey());
         }
 
         int[] res = new int[k];
         int index = 0;
-        for (int i = list.length - 1; i > 0 && index < k; i--) {
-            for (int n : list[i]) {
+        for (int i = arr.length - 1; i > 0 && index < k; i--) {
+            for (int n : arr[i]) {
                 res[index++] = n;
                 if (index == k) {
                     return res;
