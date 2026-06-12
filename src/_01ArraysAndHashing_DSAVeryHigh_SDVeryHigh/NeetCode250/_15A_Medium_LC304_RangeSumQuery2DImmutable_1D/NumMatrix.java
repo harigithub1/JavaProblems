036264faba54457966eed1b856fa5e7ff2a40411
Hari <69260548+harigithub1@users.noreply.github.com>
2026-06-12 06:10:29 +0530

    Fix loop condition for prefix sum calculation

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
@@ -8,7 +8,7 @@ public class NumMatrix {
         prefixSum = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             prefixSum[i][0] = matrix[i][0];
-            for (int j = 1; j < cols; j++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 prefixSum[i][j] = prefixSum[i][j - 1] + matrix[i][j];
             }
         }
