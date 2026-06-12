bb17ac19dad39430ba35a09b73240f35838f217b
Hari <69260548+harigithub1@users.noreply.github.com>
2026-06-12 06:09:45 +0530

    Fix loop initialization in NumMatrix constructor

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
@@ -6,7 +6,7 @@ public class NumMatrix {
 
     public NumMatrix(int[][] matrix) {
         prefixSum = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < rows; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             prefixSum[i][0] = matrix[i][0];
             for (int j = 1; j < cols; j++) {
                 prefixSum[i][j] = prefixSum[i][j - 1] + matrix[i][j];
