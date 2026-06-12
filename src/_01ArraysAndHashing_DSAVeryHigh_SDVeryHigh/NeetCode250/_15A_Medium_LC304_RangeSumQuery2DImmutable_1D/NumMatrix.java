774c71dad773e3c397ce07c87bb4fdbadf4fdafc
Hari <69260548+harigithub1@users.noreply.github.com>
2026-06-12 06:08:49 +0530

    Refactor NumMatrix for improved readability

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15A_Medium_LC304_RangeSumQuery2DImmutable_1D/NumMatrix.java
@@ -5,23 +5,22 @@ public class NumMatrix {
     private int[][] prefixSum;
 
     public NumMatrix(int[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
-        prefixSum = new int[rows][cols];
-        for (int row = 0; row < rows; row++) {
-            prefixSum[row][0] = matrix[row][0];
-            for (int col = 1; col < cols; col++) {
-                prefixSum[row][col] = prefixSum[row][col - 1] + matrix[row][col];
+        prefixSum = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < rows; i++) {
+            prefixSum[i][0] = matrix[i][0];
+            for (int j = 1; j < cols; j++) {
+                prefixSum[i][j] = prefixSum[i][j - 1] + matrix[i][j];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int res = 0;
-        for (int row = row1; row <= row2; row++) {
+        for (int i = row1; i <= row2; i++) {
             if (col1 > 0) {
-                res += prefixSum[row][col2] - prefixSum[row][col1 - 1];
+                res = res + prefixSum[i][col2] - prefixSum[i][col1 - 1];
             } else {
-                res += prefixSum[row][col2];
+                res = res + prefixSum[i][col2];
             }
         }
         return res;
