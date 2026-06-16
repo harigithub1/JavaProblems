772c4a3166a7dd886752a30d2ae4b521e160f666
harigithub1 <m.haribabu26@gmail.com>
2026-06-16 07:40:03 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_01_Easy_LC1929_ConcatenationofArray/LC1929_ConcatenationofArray.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_01_Easy_LC1929_ConcatenationofArray/LC1929_ConcatenationofArray.java
@@ -2,7 +2,7 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._01_Easy_LC1929_C
 
 import java.util.Arrays;
 
-public class _01_Easy_LC1929_ConcatenationofArray {
+public class LC1929_ConcatenationofArray {
     public static int[] getConcatenation(int[] nums) {
         int l = nums.length;
         int[] res = new int[l*2];
