592a831d3a111df195a3caedd9d952b3a8bd1a84
harigithub1 <m.haribabu26@gmail.com>
2026-06-17 06:03:07 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/_06_Medium_LC049_GroupAnagrams_Beginner.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/_06_Medium_LC049_GroupAnagrams_Beginner.java
@@ -6,13 +6,13 @@ public class _06_Medium_LC049_GroupAnagrams_Beginner {
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = new String(c);
             map.putIfAbsent(key, new ArrayList<>());
             map.get(key).add(s);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList<List<String>>(map.values());
     }
 
     public static void main(String[] args) {
