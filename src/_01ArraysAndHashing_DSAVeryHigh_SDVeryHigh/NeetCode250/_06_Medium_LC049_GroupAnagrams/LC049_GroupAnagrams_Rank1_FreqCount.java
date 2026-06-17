5890285c225b863e32c1a9c9c0f3d837e31f0fb4
harigithub1 <m.haribabu26@gmail.com>
2026-06-17 06:09:21 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/LC049_GroupAnagrams_Rank1_FreqCount.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/LC049_GroupAnagrams_Rank1_FreqCount.java
@@ -2,7 +2,7 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._06_Medium_LC049_
 
 import java.util.*;
 
-public class _06_Medium_LC049_GroupAnagrams {
+public class LC049_GroupAnagrams_Rank1_FreqCount {
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
         for (String s : strs) {
--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/LC049_GroupAnagrams_Rank2_Sorting.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/LC049_GroupAnagrams_Rank2_Sorting.java
@@ -1,7 +1,7 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._06_Medium_LC049_GroupAnagrams;
 import java.util.*;
 
-public class _06_Medium_LC049_GroupAnagrams_Beginner {
+public class LC049_GroupAnagrams_Rank2_Sorting {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
