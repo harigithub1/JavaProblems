8c307e6a5b616c03878bfe1986899d394c33430e
harigithub1 <m.haribabu26@gmail.com>
2026-06-18 06:20:51 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_07_Easy_LC027_RemoveElement/LC027_RemoveElement.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_07_Easy_LC027_RemoveElement/LC027_RemoveElement.java
@@ -20,5 +20,3 @@ public class LC027_RemoveElement {
         }
     }
 }
-
-
--- src/practice/Main.java
+++
@@ -1,6 +0,0 @@
-package practice;
-class Main{
-    public static void main(String[] args) {
-        MyHashMap map = new MyHashMap();
-    }
-}
