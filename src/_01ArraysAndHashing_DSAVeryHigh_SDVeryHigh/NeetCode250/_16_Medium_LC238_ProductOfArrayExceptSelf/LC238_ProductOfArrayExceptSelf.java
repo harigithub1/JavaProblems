cf1b410809852a599b703139022071ba9c470790
harigithub1 <m.haribabu26@gmail.com>
2026-06-23 08:49:26 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_16_Medium_LC238_ProductOfArrayExceptSelf/LC238_ProductOfArrayExceptSelf.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_16_Medium_LC238_ProductOfArrayExceptSelf/LC238_ProductOfArrayExceptSelf.java
@@ -3,51 +3,36 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._16_Medium_LC238_
 public class LC238_ProductOfArrayExceptSelf {
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-        // Step 1: Prefix product
-        result[0] = 1;
+
+        //creating res array to store prefix product values initially
+        int[] res = new int[n];
+        res[0] = 1;
         for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        // Step 2: Suffix product (from right)
+        // now res has all the prefix product values
+
+        // suffix values are stored in suffix variable it changes in each iteration and is used to update the res array with the final product values
+        // its values changes from 1 to 4 to 12 to 24 for the given input (since we initialized first suffix value to 1)
+        //{1, 2, 3, 4} input
+        //{1, 1, 2, 6} prefix values
+        // 1  4  12 24 suffix values (its not an array)
+        // now mulitply suffix with res(i) you  get the solution
+        // dont forget to update suffix values after multiplying
+
         int suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
-            result[i] = result[i] * suffix;
-            suffix *= nums[i];
+            //below is the main logic where we multiply suffix values with prefix values since res contains prefix product
+            res[i] = res[i] * suffix;
+
+            suffix = suffix * nums[i];
         }
-        return result;
+        return res;
     }
-    // quick test
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4};
         int[] res = productExceptSelf(nums);
         for (int v : res) System.out.print(v + " "); // prints: 24 12 8 6
     }
 }
-/**
- * Prefix–Suffix (Cumulative Product) Pattern
- * <p>
- * nums = [1, 2, 3, 4]
- * <p>
- * result[0]=1
- * first for loop
- * i=1, result[1]=1*1=1
- * i=2, result[2]=1*2=2
- * i=3, result[3]=2*3=6
- * <p>
- * suffix=1
- * second for loop
- * i=3, result[3]=6*1=6, suffix=1*4=4
- * i=2, result[2]=2*4=8, suffix=4*3=12
- * i=1, result[1]=1*12=12, suffix=12*2=24
- * i=0, result[0]=1*24=24, suffix=24*1=24
- * <p>
- * 🧠 Related Patterns
- * This problem belongs to a broader category:
- * ✅ Prefix Sum / Prefix Product
- * ✅ Precomputation Arrays
- * ✅ Two-pass array traversal
- * ✅ Space optimization (using output array)
- */
-
-
