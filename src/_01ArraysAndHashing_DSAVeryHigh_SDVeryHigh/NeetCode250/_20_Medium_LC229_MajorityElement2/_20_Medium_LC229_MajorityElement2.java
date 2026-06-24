2c8475eaa467d33482bd3f37b97ca14c4b5b7fd2
Hari <69260548+harigithub1@users.noreply.github.com>
2026-06-24 12:46:25 +0530

    Update _20_Medium_LC229_MajorityElement2.java

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_20_Medium_LC229_MajorityElement2/_20_Medium_LC229_MajorityElement2.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_20_Medium_LC229_MajorityElement2/_20_Medium_LC229_MajorityElement2.java
@@ -1,6 +1,52 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._20_Medium_LC229_MajorityElement2;
 
 public class _20_Medium_LC229_MajorityElement2 {
+
+public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+
+            if (count.size() > 2) {
+                Map<Integer, Integer> newCount = new HashMap<>();
+                for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+                    if (entry.getValue() > 1) {
+                        newCount.put(entry.getKey(), entry.getValue() - 1);
+                    }
+                }
+                count = newCount;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : count.keySet()) {
+            int frequency = 0;
+            for (int num : nums) {
+                if (num == key) frequency++;
+            }
+            if (frequency > nums.length / 3) {
+                res.add(key);
+            }
+        }
+
+        return res;
+    }
+
+
+public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        System.out.println(majorityElement(nums1)); // [3]
+
+        int[] nums2 = {1};
+        System.out.println(majorityElement(nums2)); // [1]
+
+        int[] nums3 = {1, 2};
+        System.out.println(majorityElement(nums3)); // [1, 2]
+
+        int[] nums4 = {1, 1, 1, 3, 3, 2, 2, 2};
+        System.out.println(majorityElement(nums4)); // [1, 2]
+    }
 }
 
 
