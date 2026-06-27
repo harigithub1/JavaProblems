0d71881c5725d0d0f4e079cc17e5340a8dad742f
harigithub1 <m.haribabu26@gmail.com>
2026-06-27 14:42:26 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_19_Medium_LC122_BestTimetoBuyandSellStock2/OnO1_Greedy.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_19_Medium_LC122_BestTimetoBuyandSellStock2/OnO1_Greedy.java
@@ -1,14 +1,15 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._19_Medium_LC122_BestTimetoBuyandSellStock2;
 
 public class OnO1_Greedy {
-    public static int maxProfit(int[] prices) {
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit = profit + prices[i] - prices[i - 1];
+    public static int maxProfit(int[] nums) {
+        int maxProfit = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                int profit = nums[i] - nums[i - 1];
+                maxProfit = maxProfit + profit;
             }
         }
-        return profit;
+        return maxProfit;
     }
 
     public static void main(String[] args) {
@@ -25,9 +26,9 @@ public class OnO1_Greedy {
 /**
  * Pattern used: Greedy
  * Why Greedy?
- *
+ * <p>
  * At every step, you make the locally optimal choice:
- *
+ * <p>
  * If today's price is higher than yesterday's, take the profit immediately.
  * Don't wait for a potentially higher future price.
  */
