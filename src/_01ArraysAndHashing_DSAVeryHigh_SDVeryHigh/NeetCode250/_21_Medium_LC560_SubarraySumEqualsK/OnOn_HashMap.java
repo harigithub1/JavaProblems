58d5115141e6b2316762a63d1c1e2eeb8d3e1813
harigithub1 <m.haribabu26@gmail.com>
2026-06-27 14:50:48 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_21_Medium_LC560_SubarraySumEqualsK/OnOn_HashMap.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_21_Medium_LC560_SubarraySumEqualsK/OnOn_HashMap.java
@@ -2,17 +2,18 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._21_Medium_LC560_
 
 import java.util.*;
 
-public class LC560_SubarraySumEqualsK {
+public class OnOn_HashMap {
     public static int subarraySum(int[] nums, int k) {
         int res = 0, curSum = 0;
-        Map<Integer, Integer> prefixSums = new HashMap<>();
-        prefixSums.put(0, 1);
+        //map is prefixSums
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
-        for (int num : nums) {
-            curSum += num;
+        for (int n : nums) {
+            curSum += n;
             int diff = curSum - k;
-            res += prefixSums.getOrDefault(diff, 0);
-            prefixSums.put(curSum, prefixSums.getOrDefault(curSum, 0) + 1);
+            res = res + map.getOrDefault(diff, 0);
+            map.put(curSum, map.getOrDefault(curSum, 0) + 1);
         }
 
         return res;
