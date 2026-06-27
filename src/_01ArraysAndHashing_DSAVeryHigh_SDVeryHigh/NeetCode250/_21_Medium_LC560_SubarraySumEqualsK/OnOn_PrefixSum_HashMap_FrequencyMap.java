30f020db5445aa78be8ec3bda6567a3d76c82f54
harigithub1 <m.haribabu26@gmail.com>
2026-06-27 15:08:18 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_21_Medium_LC560_SubarraySumEqualsK/OnOn_PrefixSum_HashMap_FrequencyMap.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_21_Medium_LC560_SubarraySumEqualsK/OnOn_PrefixSum_HashMap_FrequencyMap.java
@@ -2,26 +2,20 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._21_Medium_LC560_
 
 import java.util.*;
 
-public class OnOn_HashMap {
+public class OnOn_PrefixSum_HashMap_FrequencyMap {
     public static int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
         // Prefix sum 0 occurs once
         map.put(0, 1);
-
         int sum = 0;
         int count = 0;
-
         for (int num : nums) {
-            sum += num;
-
+            sum = sum + num;
             if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
+                count = count + map.get(sum - k);
             }
-
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-
         return count;
     }
 
