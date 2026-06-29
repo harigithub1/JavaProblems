dfe44134ed5b9230b731276a3765dbff56d71eb6
harigithub1 <m.haribabu26@gmail.com>
2026-06-29 06:36:38 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_22_Hard_LC041_FirstMissingPositive/OnO1_InPlaceHashing.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_22_Hard_LC041_FirstMissingPositive/OnO1_InPlaceHashing.java
@@ -1,45 +1,45 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._22_Hard_LC041_FirstMissingPositive;
 
-public class LC041_FirstMissingPositive {
+public class OnO1_InPlaceHashing {
 
     public static int firstMissingPositive(int[] nums) {
-        int n = nums.length;
+        int l = nums.length;
 
         // Remove negatives
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < l; i++) {
             if (nums[i] < 0) {
                 nums[i] = 0;
             }
         }
 
-        // Mark existing numbers [1..n]
-        for (int i = 0; i < n; i++) {
+        // Mark existing numbers [1..l]
+        for (int i = 0; i < l; i++) {
             int val = Math.abs(nums[i]);
-            if (val >= 1 && val <= n) {
+            if (val >= 1 && val <= l) {
                 if (nums[val - 1] > 0) {
                     nums[val - 1] *= -1;
                 } else if (nums[val - 1] == 0) {
-                    nums[val - 1] = -1 * (n + 1);
+                    nums[val - 1] = -1 * (l + 1);
                 }
             }
         }
 
         // First unmarked index is the answer
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= l; i++) {
             if (nums[i - 1] >= 0) {
                 return i;
             }
         }
 
-        // All numbers 1..n exist
-        return n + 1;
+        // All numbers 1..l exist
+        return l + 1;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 0};
 //        int[] nums = {3, 4, -1, 1};
-//        int[] nums = {7, 8, 9, 11, 12};
-//        int[] nums = {1, 2, 3};
-        System.out.println(firstMissingPositive(nums)); // 4
+//        int[] nums = {1, 0};
+//        int[] nums = {1, 1};
+        System.out.println(firstMissingPositive(nums));
     }
 }
