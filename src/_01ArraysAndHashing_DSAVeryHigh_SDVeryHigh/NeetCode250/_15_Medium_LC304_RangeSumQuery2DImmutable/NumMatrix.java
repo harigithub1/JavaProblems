9d25511204742fc581ccd33b894695734f51c478
harigithub1 <m.haribabu26@gmail.com>
2026-06-30 06:39:14 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15_Medium_LC304_RangeSumQuery2DImmutable/NumMatrix.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15_Medium_LC304_RangeSumQuery2DImmutable/NumMatrix.java
@@ -23,22 +23,4 @@ public class NumMatrix {
         return prefixSum[row2 + 1][col2 + 1] - prefixSum[row1][col2 + 1] -
                 prefixSum[row2 + 1][col1] + prefixSum[row1][col1];
     }
-
-    public static void main(String[] args) {
-        int[][] matrix = {
-                {3, 0, 1, 4, 2},
-                {5, 6, 3, 2, 1},
-                {1, 2, 0, 1, 5},
-                {4, 1, 0, 1, 7},
-                {1, 0, 3, 0, 5}
-        };
-        NumMatrix numMatrix = new NumMatrix(matrix);
-//        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
-//        System.out.println(numMatrix.sumRegion(1, 0, 3, 2));
-//        System.out.println(numMatrix.sumRegion(1, 1, 1, 3));
-//        System.out.println(numMatrix.sumRegion(2, 3, 2, 3));
-//        System.out.println(numMatrix.sumRegion(0, 0, 4, 4));
-//        System.out.println(numMatrix.sumRegion(1, 1, 2, 2));
-        System.out.println(numMatrix.sumRegion(1, 2, 2, 4));
-    }
 }
---
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_15_Medium_LC304_RangeSumQuery2DImmutable/PrefixSum.java
@@ -0,0 +1,21 @@
+package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._15_Medium_LC304_RangeSumQuery2DImmutable;
+
+public class PrefixSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {3, 0, 1, 4, 2},
+                {5, 6, 3, 2, 1},
+                {1, 2, 0, 1, 5},
+                {4, 1, 0, 1, 7},
+                {1, 0, 3, 0, 5}
+        };
+        NumMatrix numMatrix = new NumMatrix(matrix);
+        System.out.println(numMatrix.sumRegion(1, 2, 2, 4));
+//        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
+//        System.out.println(numMatrix.sumRegion(1, 0, 3, 2));
+//        System.out.println(numMatrix.sumRegion(1, 1, 1, 3));
+//        System.out.println(numMatrix.sumRegion(2, 3, 2, 3));
+//        System.out.println(numMatrix.sumRegion(0, 0, 4, 4));
+//        System.out.println(numMatrix.sumRegion(1, 1, 2, 2));
+    }
+}
