dc53762205eae2e40ef5f3306c4118f1dda12961
harigithub1 <m.haribabu26@gmail.com>
2026-07-02 05:29:39 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/_Rank1_OmxnOmxn_FreqCount.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_06_Medium_LC049_GroupAnagrams/_Rank1_OmxnOmxn_FreqCount.java
@@ -4,25 +4,22 @@ import java.util.*;
 
 public class _Rank1_OmxnOmxn_FreqCount {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
             int[] count = new int[26];
             for (char c : s.toCharArray()) {
                 count[c - 'a']++;
             }
             String key = Arrays.toString(count);
-            res.putIfAbsent(key, new ArrayList<>());
-            res.get(key).add(s);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
         }
-        return new ArrayList<List<String>>(res.values());
+        return new ArrayList<>(map.values());
     }
 
     public static void main(String[] args) {
-        String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        List<List<String>> result = groupAnagrams(input);
-        for (List<String> group : result) {
-            System.out.println(group);
-        }
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
     }
 }
 
