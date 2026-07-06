6bdd96fc7565e50faf7b45c64ad7108354e7afc4
harigithub1 <m.haribabu26@gmail.com>
2026-07-06 07:12:44 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_18_Medium_LC128_LongestConsecutiveSequence/_OnOn_Hashing_HashSet.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_18_Medium_LC128_LongestConsecutiveSequence/_OnOn_Hashing_HashSet.java
@@ -9,27 +9,26 @@ public class _OnOn_Hashing_HashSet {
         for (int n : nums) {
             set.add(n);
         }
-        int longest = 0;
+        int maxLength = 0;
         for (int n : set) {
             // Only start if it's the beginning of a sequence
             if (!set.contains(n - 1)) {
-                int currentNum = n;
-                int count = 1;
+                int startPointer = n;
+                int length = 1;
                 // Expand sequence
-                while (set.contains(currentNum + 1)) {
-                    count++;
-                    currentNum++;
+                while (set.contains(startPointer + 1)) {
+                    length++;
+                    startPointer++;
                 }
-                longest = Math.max(longest, count);
+                maxLength = Math.max(maxLength, length);
             }
         }
-        return longest;
+        return maxLength;
     }
     public static void main(String[] args) {
-        int[] nums1 = {100, 4, 200, 1, 3, 2};
-//        int[] nums2 = {0, -1, 1, 2, -2, 3, 4};
-        System.out.println(longestConsecutive(nums1)); // 4
-//        System.out.println(longestConsecutive(nums2)); // 7
+        int[] nums = {100, 4, 200, 1, 3, 2};
+//        int[] nums = {0, -1, 1, 2, -2, 3, 4};
+        System.out.println(longestConsecutive(nums));
     }
 }
 
