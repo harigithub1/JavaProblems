ac9231321f9fd409b1ba00ca6f8ed116cd0cdfb7
harigithub1 <m.haribabu26@gmail.com>
2026-07-09 10:41:03 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_19_Medium_LC122_BestTimetoBuyandSellStock2/OnO1_Greedy.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_19_Medium_LC122_BestTimetoBuyandSellStock2/OnO1_Greedy.java
@@ -1,26 +1,20 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._19_Medium_LC122_BestTimetoBuyandSellStock2;
 
 public class OnO1_Greedy {
-    public static int maxProfit(int[] nums) {
-        int totalProfit = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > nums[i - 1]) {
-                int dayProfit = nums[i] - nums[i - 1];
-                totalProfit = totalProfit + dayProfit;
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                //prices[i] - prices[i - 1] is day profit
+                profit = profit + prices[i] - prices[i - 1];
             }
         }
-        return totalProfit;
+        return profit;
     }
 
     public static void main(String[] args) {
         int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(prices)); // 7
-
-//        int[] prices2 = {1, 2, 3, 4, 5};
-//        System.out.println(maxProfit(prices2)); // 4
-//
-//        int[] prices3 = {7, 6, 4, 3, 1};
-//        System.out.println(maxProfit(prices3)); // 0
     }
 }
 /**
