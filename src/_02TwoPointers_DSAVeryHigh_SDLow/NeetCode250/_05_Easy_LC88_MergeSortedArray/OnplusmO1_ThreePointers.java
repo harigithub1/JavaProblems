d14eedc533b27769547a70539e17f8842b1ecb05
harigithub1 <m.haribabu26@gmail.com>
2026-07-11 06:05:53 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_05_Easy_LC88_MergeSortedArray/OnplusmO1_ThreePointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_05_Easy_LC88_MergeSortedArray/OnplusmO1_ThreePointers.java
@@ -2,25 +2,26 @@ package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._05_Easy_LC88_MergeSortedAr
 
 public class OnplusmO1_ThreePointers {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int last = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        // Merge in reverse order
-        while (m > 0 && n > 0) {
-            if (nums1[m - 1] > nums2[n - 1]) {
-                nums1[last] = nums1[m - 1];
-                m--;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
             } else {
-                nums1[last] = nums2[n - 1];
-                n--;
+                nums1[k] = nums2[j];
+                j--;
             }
-            last--;
+            k--;
         }
 
-        // Fill nums1 with leftover nums2 elements
-        while (n > 0) {
-            nums1[last] = nums2[n - 1];
-            n--;
-            last--;
+        // Copy any remaining elements from nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 }
