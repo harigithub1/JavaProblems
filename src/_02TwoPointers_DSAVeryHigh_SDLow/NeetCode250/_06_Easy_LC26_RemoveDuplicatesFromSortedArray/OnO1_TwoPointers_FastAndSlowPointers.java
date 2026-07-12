26469dbc8318347f6a4af3b8acc55e18e3ba0740
harigithub1 <m.haribabu26@gmail.com>
2026-07-12 06:36:51 +0530

    fix

---
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_06_Easy_LC26_RemoveDuplicatesFromSortedArray/OnO1_TwoPointers_FastAndSlowPointers.java
@@ -0,0 +1,25 @@
+package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._06_Easy_LC26_RemoveDuplicatesFromSortedArray;
+
+public class OnO1_TwoPointers_FastAndSlowPointers {
+    public static int removeDuplicatesSortedArr(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;                // move unique pointer
+                nums[i] = nums[j];  // place next unique element
+            }
+        }
+        return i + 1; // length = number of unique elements
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k = removeDuplicatesSortedArr(nums);
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
