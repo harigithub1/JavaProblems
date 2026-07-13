cdca91e7f9141b25babb97ca9ae9708244b3ac65
harigithub1 <m.haribabu26@gmail.com>
2026-07-13 15:52:01 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_06_Easy_LC26_RemoveDuplicatesFromSortedArray/OnO1_TwoPointers_FastAndSlowPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_06_Easy_LC26_RemoveDuplicatesFromSortedArray/OnO1_TwoPointers_FastAndSlowPointers.java
@@ -5,12 +5,13 @@ public class OnO1_TwoPointers_FastAndSlowPointers {
         if (nums.length == 0) {
             return 0;
         }
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
                 i++;
                 nums[i] = nums[j];
             }
+            j++;
         }
         return i + 1;
     }
