4a62dee041871fec70b50e0b29c6bc785ccbf66a
harigithub1 <m.haribabu26@gmail.com>
2026-07-14 07:21:09 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_08_Medium_LC15_3Sum/On2O1_TwoPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_08_Medium_LC15_3Sum/On2O1_TwoPointers.java
@@ -1,4 +1,42 @@
 package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._08_Medium_LC15_3Sum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class On2O1_TwoPointers {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
 }
