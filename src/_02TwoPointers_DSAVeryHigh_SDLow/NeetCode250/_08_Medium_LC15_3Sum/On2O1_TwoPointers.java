a7b9e1ea1075f28fd2ef38048bf25aaa602dfc39
harigithub1 <m.haribabu26@gmail.com>
2026-07-15 06:09:49 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_08_Medium_LC15_3Sum/On2O1_TwoPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_08_Medium_LC15_3Sum/On2O1_TwoPointers.java
@@ -4,35 +4,42 @@ import java.util.*;
 
 public class On2O1_TwoPointers {
     public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Early termination
             if (nums[i] > 0) {
                 break;
             }
+            // Skip duplicate first elements
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            //initialize two pointers
-            int l = i + 1, r = nums.length - 1;
+            int l = i + 1;
+            int r = nums.length - 1;
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
-                if (sum > 0) {
-                    r--;
-                } else if (sum < 0) {
-                    l++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
                     l++;
                     r--;
+                    // Skip duplicates
                     while (l < r && nums[l] == nums[l - 1]) {
                         l++;
                     }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
                 }
             }
         }
-        return res;
+        return result;
     }
+
 
     public static void main(String[] args) {
         int[] nums = {-1, 0, 1, 2, -1, -4};
