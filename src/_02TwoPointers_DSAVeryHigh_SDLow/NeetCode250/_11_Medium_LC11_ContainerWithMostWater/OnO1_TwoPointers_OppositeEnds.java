b52fa9a1fa7f4e93c91f8f176158158ce3ccbc55
harigithub1 <m.haribabu26@gmail.com>
2026-07-19 09:16:49 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_11_Medium_LC11_ContainerWithMostWater/OnO1_TwoPointers_OppositeEnds.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_11_Medium_LC11_ContainerWithMostWater/OnO1_TwoPointers_OppositeEnds.java
@@ -4,17 +4,18 @@ public class OnO1_TwoPointers_OppositeEnds {
     public int maxArea(int[] heights) {
         int l = 0;
         int r = heights.length - 1;
-        int res = 0;
+        int maxArea = 0;
         while (l < r) {
-            int area = Math.min(heights[l], heights[r]) * (r - l);
-            res = Math.max(res, area);
-            if (heights[l] <= heights[r]) {
+            int w = r - l;
+            int h = Math.min(heights[l], heights[r]);
+            maxArea = Math.max(maxArea, w * h);
+            if (heights[l] < heights[r]) {
                 l++;
             } else {
                 r--;
             }
         }
-        return res;
+        return maxArea;
     }
 
     public static void main(String[] args) {
