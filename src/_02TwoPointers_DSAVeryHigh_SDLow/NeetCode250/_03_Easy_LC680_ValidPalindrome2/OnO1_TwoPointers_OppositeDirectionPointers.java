3ca0e9e76898d158b4785c1732c2f44335d323a3
harigithub1 <m.haribabu26@gmail.com>
2026-07-26 05:06:25 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_03_Easy_LC680_ValidPalindrome2/OnO1_TwoPointers_OppositeDirectionPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_03_Easy_LC680_ValidPalindrome2/OnO1_TwoPointers_OppositeDirectionPointers.java
@@ -1,6 +1,17 @@
 package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._03_Easy_LC680_ValidPalindrome2;
 
 public class OnO1_TwoPointers_OppositeDirectionPointers {
+    private static boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
     public static boolean validPalindrome2(String s) {
         int l = 0, r = s.length() - 1;
         while (l < r) {
@@ -11,17 +22,6 @@ public class OnO1_TwoPointers_OppositeDirectionPointers {
             r--;
         }
         //below return is if String is already a palindrome example: racecar. The loop finishes without entering the if.
-        return true;
-    }
-
-    private static boolean isPalindrome(String s, int l, int r) {
-        while (l < r) {
-            if (s.charAt(l) != s.charAt(r)) {
-                return false;
-            }
-            l++;
-            r--;
-        }
         return true;
     }
 
