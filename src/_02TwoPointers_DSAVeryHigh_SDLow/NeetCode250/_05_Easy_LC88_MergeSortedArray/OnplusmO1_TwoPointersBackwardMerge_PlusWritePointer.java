513c8c7852e9a8846b2049b79b0b08e26319962c
harigithub1 <m.haribabu26@gmail.com>
2026-07-26 07:35:03 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_05_Easy_LC88_MergeSortedArray/OnplusmO1_TwoPointersBackwardMerge_PlusWritePointer.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_05_Easy_LC88_MergeSortedArray/OnplusmO1_TwoPointersBackwardMerge_PlusWritePointer.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 
 public class OnplusmO1_TwoPointersBackwardMerge_PlusWritePointer {
     public static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
+        int r1 = m - 1;
+        int r2 = n - 1;
         int k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
+        while (r1 >= 0 && r2 >= 0) {
+            if (nums1[r1] > nums2[r2]) {
+                nums1[k] = nums1[r1];
+                r1--;
             } else {
-                nums1[k] = nums2[j];
-                j--;
+                nums1[k] = nums2[r2];
+                r2--;
             }
             k--;
         }
         // Copy any remaining elements from nums2
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
+        while (r2 >= 0) {
+            nums1[k] = nums2[r2];
+            r2--;
             k--;
         }
     }
