f533c252f9f233a5edaf303094c7104f6c309bf3
harigithub1 <m.haribabu26@gmail.com>
2026-07-26 07:57:09 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_06_Easy_LC26_RemoveDuplicatesFromSortedArray/OnO1_TwoPointers_FastAndSlowPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_06_Easy_LC26_RemoveDuplicatesFromSortedArray/OnO1_TwoPointers_FastAndSlowPointers.java
@@ -1,26 +1,26 @@
 package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._06_Easy_LC26_RemoveDuplicatesFromSortedArray;
 
 public class OnO1_TwoPointers_FastAndSlowPointers {
-    public static int removeDuplicatesSortedArr(int[] nums) {
+    public static int removeDuplicatesAndGetUniqueCount(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-        int i = 0, j = 1;
-        while (j < nums.length) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+        int l1 = 0, l2 = 1;
+        while (l2 < nums.length) {
+            if (nums[l1] != nums[l2]) {
+                l1++;
+                nums[l1] = nums[l2];
             }
-            j++;
+            l2++;
         }
-        return i + 1;
+        return l1 + 1;
     }
 
     public static void main(String[] args) {
         int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int k = removeDuplicatesSortedArr(nums);
+        int k = removeDuplicatesAndGetUniqueCount(nums);
         for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
+            System.out.print(nums[i]);
         }
     }
 }
