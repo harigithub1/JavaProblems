a33654232c483a28d73aa427c8669141d6f6c1af
harigithub1 <m.haribabu26@gmail.com>
2026-08-10 07:58:57 +0530

    fix

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_01_Easy_LC1929_ConcatenationofArray/OnOn_ArrayTraversal.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_01_Easy_LC1929_ConcatenationofArray/OnOn_ArrayTraversal.java
@@ -1,23 +1,21 @@
 package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._01_Easy_LC1929_ConcatenationofArray;
 
-import java.util.Arrays;
-
 public class OnOn_ArrayTraversal {
-    public static int[] getConcatenation(int[] nums) {
-        int l = nums.length;
-        int[] res = new int[l*2];
-        for (int i = 0; i < l; i++) {
+    public static int[] getConcatenatedArray(int[] nums) {
+        int length = nums.length;
+        int[] res = new int[length *2];
+        for (int i = 0; i < length; i++) {
             res[i] = nums[i];
-            res[i + l] = nums[i];
+            res[i + length] = nums[i];
         }
         return res;
     }
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 1};
-        int[] result = getConcatenation(nums);
-        for (int num : result) {
-            System.out.print(num + " ");
+        int[] res = getConcatenatedArray(nums);
+        for (int n : res) {
+            System.out.print(n + " ");
         }
     }
 }
