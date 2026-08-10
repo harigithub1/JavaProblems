817b81acfa609ce65ec8fb9c233f7ddc9d2877af
harigithub1 <m.haribabu26@gmail.com>
2026-08-10 07:32:10 +0530

    fix

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_13_Hard_LC42_TrappingRainWater/OnO1_TwoPointers_OppositeDirectionPointers_Greedy_CurrentBoundary.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_13_Hard_LC42_TrappingRainWater/OnO1_TwoPointers_OppositeDirectionPointers_Greedy_CurrentBoundary.java
@@ -1,7 +1,7 @@
 package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._13_Hard_LC42_TrappingRainWater;
 
 public class OnO1_TwoPointers_OppositeDirectionPointers_Greedy_CurrentBoundary {
-    public static int trap(int[] height) {
+    public static int noOfUnitsOfWaterTrapped(int[] height) {
         int l = 0;
         int r = height.length - 1;
         int leftMax = 0;
@@ -29,6 +29,6 @@ public class OnO1_TwoPointers_OppositeDirectionPointers_Greedy_CurrentBoundary {
 
     public static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.println(trap(height));
+        System.out.println(noOfUnitsOfWaterTrapped(height));
     }
 }
