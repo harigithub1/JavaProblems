a5599c2380e34a0a7825723c0b1e13d8e62c2a02
haribabumaila <m.haribabu26@gmail.com>
2026-08-25 18:40:41 +0530

    fixes

--- src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_22_Hard_LC041_FirstMissingPositive/_Rank1_OnO1_InPlaceHashing_V2.java
+++ src/_01ArraysAndHashing_DSAVeryHigh_SDVeryHigh/NeetCode250/_22_Hard_LC041_FirstMissingPositive/_Rank1_OnO1_InPlaceHashing_V2.java
@@ -2,7 +2,7 @@ package _01ArraysAndHashing_DSAVeryHigh_SDVeryHigh.NeetCode250._22_Hard_LC041_Fi
 
 public class _Rank1_OnO1_InPlaceHashing_V2 {
     public static int firstMissingPositive(int[] nums) {
-        // Replace negative values with n + 1
+        // Replace negative values and 0s with n + 1
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] <= 0) {
                 nums[i] = nums.length + 1;
