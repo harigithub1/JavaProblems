9b2901f5852275c0119410a111fdd68502e15e71
haribabumaila <m.haribabu26@gmail.com>
2026-08-26 07:09:46 +0530

    fixes

--- src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_01_Easy_LC344_ReverseString/OnO1_TwoPointers_OppositeDirectionPointers.java
+++ src/_02TwoPointers_DSAVeryHigh_SDLow/NeetCode250/_01_Easy_LC344_ReverseString/OnO1_TwoPointers_OppositeDirectionPointers.java
@@ -1,25 +1,23 @@
 package _02TwoPointers_DSAVeryHigh_SDLow.NeetCode250._01_Easy_LC344_ReverseString;
 
-import java.util.*;
-
 public class OnO1_TwoPointers_OppositeDirectionPointers {
-    public static void reverseString(char[] chars) {
+    public static void reverseString(char[] s) {
         int l = 0;
-        int r = chars.length - 1;
+        int r = s.length - 1;
         while (l < r) {
-            char temp = chars[l];
-            chars[l] = chars[r];
-            chars[r] = temp;
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
             l++;
             r--;
         }
     }
 
     public static void main(String[] args) {
-        char[] chars = {'h', 'e', 'l', 'l', 'o'};
-        reverseString(chars);
-        for (int i = 0; i < chars.length; i++) {
-            System.out.print(chars[i] + " ");
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        for(char c : s) {
+            System.out.print(c);
         }
     }
 }
