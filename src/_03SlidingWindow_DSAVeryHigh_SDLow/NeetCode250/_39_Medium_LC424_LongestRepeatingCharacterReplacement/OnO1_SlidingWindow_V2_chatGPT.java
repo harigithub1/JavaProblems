4ba24770e0a6823d24d8ae2b7d10b01e796fcb62
haribabumaila <m.haribabu26@gmail.com>
2026-09-11 10:03:43 +0530

    fixes

--- src/_03SlidingWindow_DSAVeryHigh_SDLow/NeetCode250/_39_Medium_LC424_LongestRepeatingCharacterReplacement/OnO1_SlidingWindow_V2_chatGPT.java
+++ src/_03SlidingWindow_DSAVeryHigh_SDLow/NeetCode250/_39_Medium_LC424_LongestRepeatingCharacterReplacement/OnO1_SlidingWindow_V2_chatGPT.java
@@ -2,19 +2,26 @@ package _03SlidingWindow_DSAVeryHigh_SDLow.NeetCode250._39_Medium_LC424_LongestR
 
 public class OnO1_SlidingWindow_V2_chatGPT {
     public static int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
         int l = 0;
+        int[] freq = new int[26];
         int maxFreq = 0;
         int maxLength = 0;
         for (int r = 0; r < s.length(); r++) {
             int index = s.charAt(r) - 'A';
             freq[index]++;
             maxFreq = Math.max(maxFreq, freq[index]);
-            // r - l + 1 - maxFreq => Characters that must be replaced
+            // If window needs more than k replacements
             if (r - l + 1 - maxFreq > k) {
                 int leftIndex = s.charAt(l) - 'A';
+                // Remove l character
                 freq[leftIndex]--;
+                // Move l
                 l++;
+                // Recalculate maxFreq
+                maxFreq = 0;
+                for (int i = 0; i < 26; i++) {
+                    maxFreq = Math.max(maxFreq, freq[i]);
+                }
             }
             maxLength = Math.max(maxLength, r - l + 1);
         }
--- src/_03SlidingWindow_DSAVeryHigh_SDLow/NeetCode250/_39_Medium_LC424_LongestRepeatingCharacterReplacement/OnO1_SlidingWindow_V3_chatGPT.java
+++ src/_03SlidingWindow_DSAVeryHigh_SDLow/NeetCode250/_39_Medium_LC424_LongestRepeatingCharacterReplacement/OnO1_SlidingWindow_V3_chatGPT.java
@@ -1,27 +1,20 @@
 package _03SlidingWindow_DSAVeryHigh_SDLow.NeetCode250._39_Medium_LC424_LongestRepeatingCharacterReplacement;
 
-public class OnO1_SlidingWindow_V1_chatGPT {
+public class OnO1_SlidingWindow_V3_chatGPT {
     public static int characterReplacement(String s, int k) {
-        int l = 0;
         int[] freq = new int[26];
+        int l = 0;
         int maxFreq = 0;
         int maxLength = 0;
         for (int r = 0; r < s.length(); r++) {
             int index = s.charAt(r) - 'A';
             freq[index]++;
             maxFreq = Math.max(maxFreq, freq[index]);
-            // If window needs more than k replacements
+            // r - l + 1 - maxFreq => Characters that must be replaced
             if (r - l + 1 - maxFreq > k) {
                 int leftIndex = s.charAt(l) - 'A';
-                // Remove l character
                 freq[leftIndex]--;
-                // Move l
                 l++;
-                // Recalculate maxFreq
-                maxFreq = 0;
-                for (int i = 0; i < 26; i++) {
-                    maxFreq = Math.max(maxFreq, freq[i]);
-                }
             }
             maxLength = Math.max(maxLength, r - l + 1);
         }
